9951dd8420b79863a54e28a9cb41620d8ea0b196
joaosilva <jv_rss@hotmail.com>
2020-01-02 11:15:42 -0300

    injecting values from properties file

--- src/main/java/com/study/spring_demo_annotations/app/SwimJavaConfigDemoApp.java
+++ src/main/java/com/study/spring_demo_annotations/app/SwimJavaConfigDemoApp.java
@@ -5,6 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.study.spring_demo_annotations.config.SportConfig;
 import com.study.spring_demo_annotations.model.Coach;
+import com.study.spring_demo_annotations.model.SwimCoach;
 
 public class SwimJavaConfigDemoApp {
 
@@ -14,13 +15,17 @@ public class SwimJavaConfigDemoApp {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
 		
 		// Get the bean from Spring Container
-		Coach theCoach = context.getBean("swimCoach", Coach.class);
+		SwimCoach theCoach = context.getBean("swimCoach", SwimCoach.class);
 		
 		// Call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		
 		// Call method to get the daily fortune
 		System.out.println(theCoach.getDailyFortune());
+		
+		// Call our new swim coach methods ... has the props values injected
+		System.out.println("email: " + theCoach.getEmail());
+		System.out.println("team: " + theCoach.getTeam());
 		
 		// Close the context
 		context.close();
--- src/main/java/com/study/spring_demo_annotations/config/SportConfig.java
+++ src/main/java/com/study/spring_demo_annotations/config/SportConfig.java
@@ -3,6 +3,7 @@ package com.study.spring_demo_annotations.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import com.study.spring_demo_annotations.model.Coach;
 import com.study.spring_demo_annotations.model.SwimCoach;
@@ -11,6 +12,7 @@ import com.study.spring_demo_annotations.service.SadFortuneService;
 
 @Configuration
 //@ComponentScan("com.study.spring_demo_annotations")
+@PropertySource("classpath:sport.properties")
 public class SportConfig {
 	
 	// Define bean for our sad fortune service
