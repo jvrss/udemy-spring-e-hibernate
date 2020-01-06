0e83b49e2e7f59596764200da8bce3e60e820925
joaosilva <jv_rss@hotmail.com>
2020-01-06 13:52:02 -0300

    spring mvc using model

--- WebContent/WEB-INF/view/helloworld-form.jsp
+++ WebContent/WEB-INF/view/helloworld-form.jsp
@@ -4,7 +4,7 @@
 <title>Hello World - Input Form</title>
 </head>
 <body>
-	<form action="processForm" method="GET">
+	<form action="processFormVersionTwo" method="GET">
 		<input type="text" name="studentName"
 		placeholder="What's your name?" />
 		
--- src/com/study/springdemo/controller/HelloWorldController.java
+++ src/com/study/springdemo/controller/HelloWorldController.java
@@ -1,6 +1,9 @@
 package com.study.springdemo.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,5 +21,21 @@ public class HelloWorldController {
 		return "helloworld";
 	}
 	
+	// New a controller method to read form data and 
+	// add data to the model
+	@RequestMapping("/processFormVersionTwo")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+		
+		// Read the request parameter from the HTML form
+		String theName = request.getParameter("studentName");
+		// Convert the data to all caps
+		theName = theName.toUpperCase();
+		// Create the message
+		String result  = "Yo! " + theName;
+		// Add message to model
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
 
 }
