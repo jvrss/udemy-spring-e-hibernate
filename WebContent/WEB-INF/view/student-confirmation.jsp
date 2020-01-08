688d842fa17969f192b1765fc97cabbed0cb60f9
joaosilva <jv_rss@hotmail.com>
2020-01-08 16:20:24 -0300

    spring mvc drop down list in html

--- WebContent/WEB-INF/view/student-confirmation.jsp
+++ WebContent/WEB-INF/view/student-confirmation.jsp
@@ -6,5 +6,9 @@
 	</head>
 	<body>
 		The student is confirmed: ${student.firstName} ${student.lastName}
+		
+		<br><br>
+		
+		Country: ${student.country}
 	</body>
 </html>
--- src/com/study/springdemo/model/Student.java
+++ src/com/study/springdemo/model/Student.java
@@ -1,5 +1,7 @@
 package com.study.springdemo.model;
 
+import java.util.LinkedHashMap;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +9,21 @@ public class Student {
 	
 	private String firstName;
 	private String lastName;
+	
+	private String country;
+	
+	private LinkedHashMap<String, String> countryOptions;
+	
+	public Student() {
+		
+		// Populate country options: used ISO country code
+		countryOptions = new LinkedHashMap<String, String>();
+		
+		countryOptions.put("BR", "Brazil");
+		countryOptions.put("FR", "France");
+		countryOptions.put("DE", "Germany");
+		countryOptions.put("IN", "India");
+	}
 	
 	public String getFirstName() {
 		return firstName;
@@ -19,6 +36,15 @@ public class Student {
 	}
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
 	}
 
 }
