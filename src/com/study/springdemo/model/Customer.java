2dece52b785e38296373bf7de6c3cd1bea2bc4f9
SoldierJVX <jv_rss@hotmail.com>
2020-02-03 22:02:17 -0300

    spring mvc validating regular expressions

--- src/com/study/springdemo/model/Customer.java
+++ src/com/study/springdemo/model/Customer.java
@@ -2,10 +2,7 @@ package com.study.springdemo.model;
 
 import lombok.Data;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 @Data
 public class Customer {
@@ -19,4 +16,7 @@ public class Customer {
     @Min(value=0, message = "must be greater than or equal to zero")
     @Max(value=10, message= "must be less than or equal to 10")
     private int freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
 }
