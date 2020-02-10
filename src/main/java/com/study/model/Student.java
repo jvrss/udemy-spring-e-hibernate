8b5c19c7ba421979ec4f7a7cddc58c8d47cc773a
SoldierJVX <jv_rss@hotmail.com>
2020-02-10 20:26:46 -0300

    hibernate - review primary keys

--- src/main/java/com/study/model/Student.java
+++ src/main/java/com/study/model/Student.java
@@ -2,10 +2,7 @@ package com.study.model;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="student")
@@ -13,6 +10,7 @@ import javax.persistence.Table;
 public class Student {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
 
