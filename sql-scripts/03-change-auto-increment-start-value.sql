204d9c02070faa2934aff3593c3be416e4152beb
SoldierJVX <jv_rss@hotmail.com>
2020-02-27 22:57:05 -0300

    Changing the auto increment start value

---
+++ sql-scripts/03-change-auto-increment-start-value.sql
@@ -0,0 +1,5 @@
+-- Change Start Value
+ALTER TABLE hb_student_tracker.student AUTO_INCREMENT=3000
+
+-- Reset to default value and cleaning all registers of table
+truncate hb_student_tracker.student
