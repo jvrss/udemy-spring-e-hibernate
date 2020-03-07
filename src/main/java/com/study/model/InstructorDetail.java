098666f19b0ed2fe5a838f2a4983812910804e6a
SoldierJVX <jv_rss@hotmail.com>
2020-03-07 14:48:38 -0300

    database models

---
+++ src/main/java/com/study/model/InstructorDetail.java
@@ -0,0 +1,44 @@
+package com.study.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="instructor_detail")
+@Data
+public class InstructorDetail {
+
+    // Annotate the class as an entity and map to db table
+
+    // Define the fields
+
+    // Annotate the fields with db column names
+
+    // Create constructors
+
+    // Generate getter/setter methods
+
+    // Generate toString() method
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name = "youtube_channel")
+    private String youtubeChannel;
+
+    @Column(name = "hobby")
+    private String hobby;
+
+    public InstructorDetail() {
+    }
+
+    public InstructorDetail(String youtubeChannel, String hobby) {
+        this.youtubeChannel = youtubeChannel;
+        this.hobby = hobby;
+    }
+
+
+}
