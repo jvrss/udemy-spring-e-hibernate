e704d9cc2516c4d3dadfda0b3f3f0edb4e5263b0
SoldierJVX <jv_rss@hotmail.com>
2020-03-15 16:40:58 -0300

    Created the Course class

---
+++ src/main/java/com/study/model/Course.java
@@ -0,0 +1,49 @@
+package com.study.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "course")
+@Data
+public class Course {
+
+    // Define our fields
+
+    // Define constructors
+
+    // Define getter setters
+
+    // define toString
+
+    // Annotate fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinColumn(name = "instructor_id")
+    private Instructor instructor;
+
+    public Course() {
+    }
+
+    public Course(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
