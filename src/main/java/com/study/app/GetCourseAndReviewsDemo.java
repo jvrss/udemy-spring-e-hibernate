9687b16dc6b09d38fe1bc165ebad309916211eb0
SoldierJVX <jv_rss@hotmail.com>
2020-04-03 16:17:56 -0300

    getting the course and reviews

---
+++ src/main/java/com/study/app/GetCourseAndReviewsDemo.java
@@ -0,0 +1,52 @@
+package com.study.app;
+
+import com.study.model.Course;
+import com.study.model.Instructor;
+import com.study.model.InstructorDetail;
+import com.study.model.Review;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class GetCourseAndReviewsDemo {
+
+    public static void main(String[] args) {
+        // Create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Review.class)
+                .buildSessionFactory();
+        // Create session
+        Session session = factory.getCurrentSession();
+
+        try{
+
+            // Start a transaction
+            session.beginTransaction();
+
+            // get the course
+            int theId = 10;
+            Course tempCourse = session.get(Course.class, theId);
+
+            // print the course
+            System.out.println(tempCourse);
+
+            // print the course reviews
+            System.out.println(tempCourse.getReviews());
+
+            // Commit transaction
+            session.getTransaction().commit();
+            System.out.println("Done!");
+        } finally {
+            // add clean up code
+            session.close();
+
+            factory.close();
+        }
+
+    }
+
+}
