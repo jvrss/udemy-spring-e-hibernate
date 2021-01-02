684f060c6d5b16b8c9698a4e495fcfa323a7a2e9
SoldierJVX <jv_rss@hotmail.com>
2021-01-02 14:38:31 -0300

    mysql driver and test connection database

--- src/main/java/com/study/TestDbServlet.java
+++ src/main/java/com/study/TestDbServlet.java
@@ -6,10 +6,41 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 @WebServlet("/")
 public class TestDbServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().println("Hello World");
+
+        // setup connection variables
+        String user = "root";
+        String pass = "master";
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL-false&serverTimezone=UTC";
+        String driver = "com.mysql.cj.jdbc.Driver";
+
+        // get connection to database
+
+        try {
+            PrintWriter out = response.getWriter();
+
+            out.println("Connection to database: " + jdbcUrl);
+
+            Class.forName(driver);
+
+            Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+
+            out.println("SUCCESS!!!");
+
+            myConn.close();
+
+        } catch (Exception exc) {
+            exc.printStackTrace();
+            throw new ServletException(exc);
+        }
+
+
     }
 }
