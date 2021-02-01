c76640bcf1f84c85706e01e8ef01f4869f8dfbda
SoldierJVX <jv_rss@hotmail.com>
2021-01-31 22:12:54 -0300

    adicionando service class

--- src/main/java/com/luv2code/springdemo/controller/CustomerController.java
+++ src/main/java/com/luv2code/springdemo/controller/CustomerController.java
@@ -2,9 +2,12 @@ package com.luv2code.springdemo.controller;
 
 import com.luv2code.springdemo.dao.CustomerDAO;
 import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -13,17 +16,15 @@ import java.util.List;
 @RequestMapping("/customer")
 public class CustomerController {
 
-    // need to inject the customer dao
+    // need to inject our customer service
     @Autowired
-    private CustomerDAO customerDAO;
+    private CustomerService customerService;
 
-
-
-    @RequestMapping("/list")
+    @GetMapping("/list")
     public String listCustomers(Model theModel){
 
-        // get customers from the dao
-        List<Customer> theCustomers = customerDAO.getCustomers();
+        // get customers from the service
+        List<Customer> theCustomers = customerService.getCustomers();
 
         // add the customers to the model
         theModel.addAttribute("customers", theCustomers);
