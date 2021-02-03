b6ebb5ebec4b8676d36ed9c142baec866e612f28
SoldierJVX <jv_rss@hotmail.com>
2021-02-02 22:37:58 -0300

    implementando atualização do cadastro

--- src/main/java/com/luv2code/springdemo/controller/CustomerController.java
+++ src/main/java/com/luv2code/springdemo/controller/CustomerController.java
@@ -6,10 +6,7 @@ import com.luv2code.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -52,6 +49,20 @@ public class CustomerController {
         customerService.saveCustomer(theCustomer);
 
         return "redirect:/customer/list";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("customerId") int theId, Model theModel){
+
+        // get the customer from our service
+        Customer theCustomer = customerService.getCustomer(theId);
+
+        // set customer as a model attribute to pre-populate the form
+        theModel.addAttribute("customer", theCustomer);
+
+        // send over to our form
+
+        return "customer-form";
     }
 
 
--- src/main/java/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ src/main/java/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -39,8 +39,20 @@ public class CustomerDAOImpl implements CustomerDAO{
         // get the current hibernate session
         Session currentSession = sessionFactory.getCurrentSession();
 
-        // save the customer ... finally LOL
-        currentSession.save(theCustomer);
+        // save/update the customer ... finally LOL
+        currentSession.saveOrUpdate(theCustomer);
 
+    }
+
+    @Override
+    public Customer getCustomer(int theId) {
+
+        // get the current hibernate session
+        Session currentSession = sessionFactory.getCurrentSession();
+
+        // now retrieve/read from database using the primary key
+        Customer theCustomer = currentSession.get(Customer.class, theId);
+
+        return theCustomer;
     }
 }
