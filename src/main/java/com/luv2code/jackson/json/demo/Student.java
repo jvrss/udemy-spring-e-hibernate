8bdef36888bd053070cf449e66b5c11e9f9b0137
SoldierJVX <jv_rss@hotmail.com>
2021-02-21 00:02:36 -0300

    simple example

---
+++ src/main/java/com/luv2code/jackson/json/demo/Student.java
@@ -0,0 +1,13 @@
+package com.luv2code.jackson.json.demo;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private boolean active;
+
+}
