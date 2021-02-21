854623aa1a12e91aab28187beb3b4c941be6cfd7
SoldierJVX <jv_rss@hotmail.com>
2021-02-21 00:15:18 -0300

    ignoring properties

--- src/main/java/com/luv2code/jackson/json/demo/Student.java
+++ src/main/java/com/luv2code/jackson/json/demo/Student.java
@@ -1,8 +1,10 @@
 package com.luv2code.jackson.json.demo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Student {
 
     private int id;
